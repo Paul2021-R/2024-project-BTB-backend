f27dc739321ae6f7118c4f17acb8c99a5b2eeda5
Ryu(Paul) <ryu.axel@gmail.com>
2024-05-10 22:50:18 +0900

    WIP: add toDo List

--- src/main/java/beyondthebracketsolo/projectbtbsolo/Application.java
+++ src/main/java/beyondthebracketsolo/projectbtbsolo/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		//TODO: Redis 연결
+		//TODO: JWT 토큰 연결
+		//TODO: 리모트 서버 설정
+		//TODO: sonar cloud 설정
+		//TODO: git action 설정(빌드 -> 배포 -> 테스트 서버 구동)
 		SpringApplication.run(Application.class, args);
 	}
 
